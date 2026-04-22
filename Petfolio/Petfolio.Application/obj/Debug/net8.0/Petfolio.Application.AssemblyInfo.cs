60993a74fbd2526715456c2e4034f5aa204cbb70
kalebtavares <kalebtechk@gmail.com>
2026-04-22 00:04:22 +0000

    chore: remove arquivo Class1.cs não utilizado
    Remove o arquivo Class1.cs do projeto Petfolio.Application, que foi
    adicionado automaticamente pelo template e não é necessário para o projeto.

--- Petfolio/Petfolio.Application/obj/Debug/net8.0/Petfolio.Application.AssemblyInfo.cs
+++ Petfolio/Petfolio.Application/obj/Debug/net8.0/Petfolio.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27badca4cd74e82304d472b9051030576044e4fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60cdc120e13264cf1405ab64a4c7df0fed5942d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
